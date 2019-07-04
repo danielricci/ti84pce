3bf29842ea90cbc0b08deae8f232482466d93c77
Daniel Ricci <thedanny09@icloud.com>
2019-07-04 08:00:14 -0400

    Program loop workflow implementation

--- Shapes/src/Shapes.asm
+++ Shapes/src/Shapes.asm
@@ -1,6 +1,57 @@
+.nolist
 #include "ti84pce.inc"
-
+.list
 .assume ADL=1
 .org userMem-2
 .db tExtTok, tAsm84CeCmp
+
+pixelX equ cmdpixelshadow
+pixelY equ pixelX+3
+color equ pixelY+1
+done equ color+1
+
+main:
+    call _HomeUp
+    call _RunIndicOff
+    call _ClrScrn
+    jr init
+clean:
+    call _ClrScrn
+    call _DrawStatusBar
+    ret
+init:
+    ld a,0
+    ld (done),a
+main_loop:
+    ld a,(done)
+    cp 1
+    jr Z,clean
+    call processInput
+    call update
+    call render
+    jr main_loop
+processInput:
+    call _getKey
+    cp kUp
+    call Z,keyUpPressed
+    cp kLeft
+    call Z,keyLeftPressed
+    cp kDown
+    call Z,keyDownPressed
+    cp kRight
+    call Z,keyRightPressed
+    cp kEnter
+    call Z,keyEnterPressed
+    ret
+keyUpPressed:
+keyLeftPressed:
+keyDownPressed:
+keyRightPressed:
+keyEnterPressed:
+    ld a,1
+    ld (done),a
+    ret
+update:
+    ret
+render:
     ret

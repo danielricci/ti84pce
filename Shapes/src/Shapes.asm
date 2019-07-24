fb5390e4c4b61d460909a71763aaabc6440d87f7
Daniel Ricci <thedanny09@icloud.com>
2019-07-23 22:29:03 -0400

    Move dot around

--- Shapes/src/Shapes.asm
+++ Shapes/src/Shapes.asm
@@ -8,48 +8,74 @@
 ; --------------------------------------------------
 ; Equates
 ; --------------------------------------------------
-done equ cmdPixelShadow
-
+done .equ cmdPixelShadow
+point .equ done+1
+color .equ $E0
 ; --------------------------------------------------
 ; Entry point of the application
 ; --------------------------------------------------
-main:
+init:
     call _HomeUp
     call _RunIndicOff
     call _ClrScrn
-    jr ctor
-; --------------------------------------------------
+    ld a,0
+    ld (done),a
+    ld hl,vRam+(lcdWidth/2)+(lcdWidth*(lcdHeight/2))
+    ld (point),hl
+    call copyHL1555Palette
+    jr mainLoop
+; -----------------------------
 ; Clean up code before the application terminates
 ; --------------------------------------------------
-clean:
+exit:
     call _ClrScrn
+    ld a,lcdBpp16
+    ld (mpLcdCtrl),a
     call _DrawStatusBar
     ret
 ; --------------------------------------------------
-; Constructs this application by loading the proper default values
+; Sets the color palette to an 8BPP format
 ; --------------------------------------------------
-ctor:
-    ld a,0
-    ld (done),a
+copyHL1555Palette:
+    ld hl,mpLcdPalette
+    ld b,0
+copyHL1555PaletteLoop:
+    ld d,b
+    ld a,b
+    and %11000000
+    srl d
+    rra
+    ld e,a
+    ld a,%00011111
+    and b
+    or e
+    ld (hl),a
+    inc hl
+    ld (hl),d
+    inc hl
+    inc b
+    jr nz,CopyHL1555PaletteLoop
+    call _boot_ClearVRAM
+    ld a,lcdBpp8
+    ld (mpLcdCtrl),a
+    ret
 ; --------------------------------------------------
 ; Clears the screen by applying the color `white` to every pixel of the lcd
 ; --------------------------------------------------
 clearScreen:
-    ld a,$FF
-    ld bc,vRamEnd-vRam
-    ld hl,vRam
-    call _MemSet
+    call _boot_ClearVRAM
+    ret
 ; --------------------------------------------------
 ; Main game loop
 ; --------------------------------------------------
-main_loop:
+mainLoop:
     ld a,(done)
     cp 1
-    jr Z,clean
+    jr Z,exit
     call processInput
     call update
     call render
-    jr main_loop
+    jr mainLoop
 ; --------------------------------------------------
 ; Processes the user input key commands
 ; 
@@ -79,22 +105,47 @@ processInput:
     call Z,keyClearPressed
     ret
 keyOnePressed: ;point
+    call clearScreen
+    ld hl,(point)
+    ld (hl),color
     ret
-keyTwoPressed: ;horizontal line
-    ret
-keyThreePressed: ;vertical line
-    ret
-keyFourPressed: ;square
-    ret
-keyEnterPressed: ;exit
-keyClearPressed: ;exit
+keyTwoPressed:
+keyThreePressed:
+keyFourPressed:
+keyEnterPressed:
+keyClearPressed:
     ld a,1
     ld (done),a
     ret
 keyUpPressed:
+    call clearScreen
+    ld hl,(point)
+    ld bc, -lcdWidth
+    add hl,bc
+    ld (point),hl
+    ld (hl),color
+    ret
 keyLeftPressed:
+    call clearScreen
+    ld hl,(point)
+    dec hl
+    ld (point),hl
+    ld (hl),color
+    ret
 keyDownPressed:
+    call clearScreen
+    ld hl,(point)
+    ld bc,lcdWidth
+    add hl,bc
+    ld (point),hl
+    ld (hl),color
+    ret
 keyRightPressed:
+    call clearScreen
+    ld hl,(point)
+    inc hl
+    ld (point),hl
+    ld (hl),color
     ret
 ; --------------------------------------------------
 ; Performs an update of the application by changing subsequent values based on

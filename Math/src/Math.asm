53de6fff9f9df3d3d7de60698cfc5c38b42248eb
Daniel Ricci <thedanny09@icloud.com>
2019-08-31 23:14:32 -0400

    Implemented multiply

--- Math/src/Math.asm
+++ Math/src/Math.asm
@@ -1,10 +1,11 @@
+;#define DEBUG
+#ifndef DEBUG
 #include "ti84pce.inc"
 .assume ADL=1
 .org userMem-2
 .db tExtTok, tAsm84CeCmp
-    ld c,4
-    ld d,2
-C_Div_D_8bit:
+#endif
+Math_Divide_C_D:
     ld b,8
     xor a
         sla c
@@ -14,4 +15,23 @@ C_Div_D_8bit:
             inc c
             sub d
     djnz $-8
+    ret
+Math_Multiply_BC_DE:
+#ifdef DEBUG
+   ld de,500
+   ld bc,500
+   ld a,24
+#endif
+    ld hl,0
+mul_loop:
+    srl b
+    rr c
+    jr nc,no_add
+    add hl,de
+no_add:
+    ex de,hl
+    add hl,hl
+    ex de,hl
+    dec a
+    jr nz,mul_loop
     ret

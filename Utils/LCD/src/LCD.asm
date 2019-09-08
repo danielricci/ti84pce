2db7b84103c872907b0c6d0d30d4888e987ac603
Daniel Ricci <thedanny09@icloud.com>
2019-09-08 00:29:07 -0400

    clear half of the LCD

--- Utils/LCD/src/LCD.asm
+++ Utils/LCD/src/LCD.asm
@@ -1,3 +1,10 @@
+;#define DEBUG
+#ifdef DEBUG
+#include "ti84pce.inc"
+.assume ADL=1
+.org userMem-2
+.db tExtTok, tAsm84CeCmp
+#endif
 LCD_CopyHL1555Palette:
     ld hl,mpLcdPalette
     ld b,0
@@ -24,4 +31,13 @@ copyHL1555Palette_Loop:
 LCD_ResetPalette:
     ld a,lcdBpp16
     ld (mpLcdCtrl),a
+    ret
+
+LCD_ClearLCDHalf:
+#ifndef DEBUG
+    ld a,$FF
+    ld bc,320*240*2
+#endif
+    ld hl,vRam
+    call _MemSet
     ret
